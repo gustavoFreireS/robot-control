b21bf79f46290ea83b18cac9b648a8e002489755
gustavoFrereS <gustavo.freire.s@outlook.com>
2019-10-05 12:00:14 -0300

    go to both sides and backward

--- roboto.py
+++ roboto.py
@@ -1,11 +1,11 @@
 from gpiozero import Robot
 import pygame
 
-" this line defines the gpio associated with the ac motors 
-robby = Robot(left=(7,8), right=(9,10))
+robby = Robot(left=(7,8), right=(10,9))
+
 
 pygame.init()
-" after initiated use pygames's joystick class to access the connected joystick
+
 j = pygame.joystick.Joystick(0)
 j.init()
 
@@ -13,11 +13,32 @@ try:
     while True:
         events = pygame.event.get()
         for event in events:
+            if event.type == pygame.JOYAXISMOTION:
+                if j.get_axis(0) < 0:
+                    robby.forward()
+                    robby.left()
+                    if j.get_button(0):
+                        robby.forward()
+                elif j.get_axis(0) > 0:
+                    robby.forward()
+                    robby.right()
+                    if j.get_button(0):
+                        robby.forward()
+                elif j.get_axis(0) == 0:
+                    if not j.get_button(0):
+                        robby.stop()
+                
+                    
             if event.type == pygame.JOYBUTTONDOWN:
                 print("Button Pressed")
-                " if someone holds b button on xbox one controller, should start the engine
-                if j.get_button(1):
+                
+                    
+                if j.get_button(0):
                     robby.forward()
+                    
+                if j.get_button(4):
+                    robby.backward()
+               
             elif event.type == pygame.JOYBUTTONUP:
                 print("Button Released")
                 robby.stop()
